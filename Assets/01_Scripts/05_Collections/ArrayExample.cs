9a84e7575c39788cb64ba26e01b1fede17f67539
JakeKwak95 <wlghrhkr@gmail.com>
2025-11-20 16:55:18 +0900

    Array Example 오류 수정

--- Assets/01_Scripts/05_Collections/ArrayExample.cs
+++ Assets/01_Scripts/05_Collections/ArrayExample.cs
@@ -11,13 +11,13 @@ public class ArrayExample : MonoBehaviour
 		InitializeArray(intArray); // 배열은 참조 타입이므로 메소드 내에서 값 변경 가능 (추후 값 타입과 참조 타입에 대해 다룸)
 
 		// 배열 요소 접근 및 출력
-		GetArrayMember(intArray, 0);
+		GetArrayMember(0);
 		ChangeArrayMemberValue(0, 100);
-		GetArrayMember(intArray, 0);
+		GetArrayMember(0);
 
 		// 배열의 길이 넘어가는 인덱스 접근 시도 시 예외(에러) 발생
 		// index 는 0부터 시작하므로 길이가 3인 배열의 유효한 인덱스는 0, 1, 2
-		GetArrayMember(intArray, 3);
+		GetArrayMember(3);
 
 		Debug.Log("메롱~"); // 메서드 내에서 예외가 발생하여 발생 지점 아래인 이 로그는 출력되지 않음
 	}
