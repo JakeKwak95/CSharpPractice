2e7f96037aba3f43d5585703d978e2588886b796
JakeKwak95 <wlghrhkr@gmail.com>
2025-11-20 13:41:09 +0900

    Data Types

---
+++ Assets/01_Scripts/01_Variables/DataTypes.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DataTypes : MonoBehaviour
+{
+	// 기본 자료형
+	int score = 0;
+	float speed = 5.5f;
+	bool isGameOver = false;
+	string playerName = "Player";
+
+	// Start는 게임이 시작될 때 실행
+	void Start()
+	{
+		Debug.Log("=== Data Types Example ===");
+		Debug.Log("Score: " + score);
+		Debug.Log("Speed: " + speed);
+		Debug.Log("Is Game Over? " + isGameOver);
+		Debug.Log("Player Name: " + playerName);
+
+		// 값 변경
+		score = 10;
+		speed = 7.2f;
+		isGameOver = true;
+		playerName = "Kwak";
+
+		Debug.Log("=== After Update ===");
+		Debug.Log($"Score: {score}, Speed: {speed}, GameOver: {isGameOver}, Name: {playerName}");
+	}
+}
