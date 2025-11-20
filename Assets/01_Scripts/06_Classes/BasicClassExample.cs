4b037ffc11683a38bd3fc25f8f70d99acc4aad82
JakeKwak95 <wlghrhkr@gmail.com>
2025-11-20 18:09:01 +0900

    Constructor

--- Assets/01_Scripts/06_Classes/BasicClassExample.cs
+++ Assets/01_Scripts/06_Classes/BasicClassExample.cs
@@ -7,6 +7,21 @@ public class BasicClass
 	public string name;
 	public int id;
 
+	// 생성자(Constructor) 정의
+	// 생성자는 클래스 이름과 동일하며, 객체가 생성될 때 호출됨
+	public BasicClass()
+	{
+		name = "DefaultName";
+		id = 0;
+	}
+
+	// 매개변수가 있는 생성자로 오버로딩
+	public BasicClass(string name, int id)
+	{
+		this.name = name; // this 키워드는 현재 객체를 가리킴
+		this.id = id; // 매개변수와 필드 이름이 같을 때 구분하기 위해 사용
+	}
+
 	// 메서드(Method) 정의
 	public void PrintStatus()
 	{
@@ -26,6 +41,10 @@ public class BasicClassExample : MonoBehaviour
 		basicClass.name = "ExampleName"; // 객채의 필드에 .을 사용하여 접근
 		basicClass.id = 1;
 		basicClass.PrintStatus(); // 클래스의 매서드 또한 .을 사용하여 접근
+
+		// 생성자 사용
+		BasicClass anotherClass = new BasicClass("AnotherName", 2);
+		anotherClass.PrintStatus();
 	}
 }
 
